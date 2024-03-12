7a476f2408afc2cea1f81157a8bf5429c68b2099
Anastasiia Kurulenko <kurulenkoanastasia@gmail.com>
2024-03-12 17:52:10 +0100

    controller logic implemented

--- src/main/java/ua/javarush/Main.java
+++ src/main/java/ua/javarush/Main.java
@@ -6,21 +6,9 @@ import ua.javarush.view.ConsoleViewProvider;
 
 public class Main {
     public static void main(String[] args) {
-//        Service engine = new Service();
-//
-//        Customer customer = engine.createCustomer(
-//                "John", "Brown",
-//                "custom@gmail.com", "Dale street, 23", "Nile",
-//                "Adana", "123-456-789");
-//
-//        engine.returnAnyInventoryToStore();
-//
-//        engine.rentAnyAvailableFilmAndMakePayment(customer);
-//
-//        Film film = engine.createFilm("TEST", "English", "English",
-//                new String[]{"Comedy", "Animation"}, 6, 3, 2.4D,
-//                34.5D);
-        Controller controller = new Controller(new Service(), new ConsoleViewProvider());
+        Service service = new Service();
+        ConsoleViewProvider consoleViewProvider = new ConsoleViewProvider();
+        Controller controller = new Controller(service, consoleViewProvider);
         controller.process();
     }
 }
